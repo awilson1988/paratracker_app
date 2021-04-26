816d222b74db5a4d5ad48ddc27d7b7224d38586d
amanda <amanda.blaze1988@gmail.com>
2021-04-26 16:47:28 -0700

    Created Cyrptid Controllers

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -8,7 +8,12 @@ class SightingsController < ApplicationController
   end
 
   def create 
-    
+    @sighting = Sighting.new(sighting_params)
+    if @sighting.save 
+      redirect_to sighting_path(@sighting)
+    else 
+      render :new
+    end
   end
 
   def show
@@ -21,6 +26,12 @@ class SightingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+
+  def sighting_params
+    params.require(:sighting).permit(:date, :location, :cryptid, :account)
   end
 end
 
