5c72f3ecec21650a6d95fff493ef8714e3963010
amanda <amanda.blaze1988@gmail.com>
2021-05-03 19:05:40 -0600

    Created Login and Signup pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    user = User.new(user_params(:username, :first_name, :last_name, :email, :passwordm))
     if user.save 
       session[:user_id] = user.id 
-    redirect_to root_path 
+    redirect_to user_path(@user) 
     else 
       render :new
     end
