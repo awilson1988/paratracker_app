3b1cb28137a8dbfdeda16e2748152157ab0d064c
amanda <amanda.blaze1988@gmail.com>
2021-05-04 16:16:02 -0600

    Fixed Delete action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by_id(params[:id])
-    @user.update(user_params(:username, :first_name, :last_name, :email))
+    @user.update(user_params)
     redirect_to user_path(@user)
   end
 
