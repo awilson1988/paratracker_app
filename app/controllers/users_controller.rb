eee2d11d5100674047fd915f6106187ce9f46677
amanda <amanda.blaze1988@gmail.com>
2021-05-04 15:29:18 -0600

    Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
     if @user.save 
       session[:user_id] = @user.id 
-    redirect_to user_path(@user.id) 
+      redirect_to user_path(@user.id) 
     else 
       render :new
     end
@@ -44,10 +44,10 @@ class UsersController < ApplicationController
   private 
 
   def user_params  
-    params.require(:user).permit(:email, :username, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
   end
 
-  def user_params(*args)
-    params.require(:user).permit(*args) 
-  end 
+  # def user_params(*args)
+  #   params.require(:user).permit(*args) 
+  # end 
 end
