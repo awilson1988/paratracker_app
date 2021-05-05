7389ab8ddf323d6711e0c68879e159beabc18579
amanda <amanda.blaze1988@gmail.com>
2021-05-04 20:31:49 -0600

    Created index and show pages for cryptids

--- app/controllers/cryptids_controller.rb
+++ app/controllers/cryptids_controller.rb
@@ -4,20 +4,40 @@ class CryptidsController < ApplicationController
   end
 
   def new
+    @cryptid = Cryptid.new
   end
 
   def create
+    @cryptid = Cryptid.new(cryptid_params)
+      if @sighting.save 
+        redirect_to @sighting
+      else 
+        render :new
+    end
   end
 
   def show
+    @cryptid = Cryptid.find_by_id(params[:id])
   end
 
   def edit
+    @cryptid = Cryptid.find_by_id(params[:id])
   end
 
   def update
+    @cryptid = Cryptid.find_by_id(params[:id]) 
+    @cryptid.update(crytid_params(:name, :description))
   end
 
   def destroy
+    @cryptid = Cryptid.find_by_id(params[:id]) 
+    @cryptid.destroy
+    redirect_to cryptids_path
+  end
+
+  private
+
+  def cryptid_params 
+    params.require(:cryptid).permit(:name, :description, :cryptid_id)
   end
 end
