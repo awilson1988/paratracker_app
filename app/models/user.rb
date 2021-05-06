90bea2a6bd8f26c141b41d71419b445ad4907d22
amanda <amanda.blaze1988@gmail.com>
2021-05-06 12:45:53 -0600

    Created Nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :username
   validates_presence_of :username, :email, :first_name, :last_name 
-  # validates_length_of :password, in: 5..15,  
+  # validates_length_of :password, length: {in: 5..15}, confirmation: true, on: :create  
 end
