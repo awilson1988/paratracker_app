2b739d253bb2ae88d231ed3e40a4631ff5a974c8
amanda <amanda.blaze1988@gmail.com>
2021-07-20 17:27:07 -0700

    Debugging CRUD for Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,35 @@
-class CommentsController < ApplicationController 
+class CommentsController < ApplicationController
+    include ApplicationHelper 
+     before_action :find_user
+     before_action :can_delete?, only: [:destroy]
     def create 
         @sighting = Sighting.find(params[:sighting_id])
-        @comment = @sighting.comments.create(params[:comment].permit(:name, :body)) 
-        redirect_to sighting_path(@sighting)
+        @comment = @sighting.comments.create(params[:comment].permit(:name, :body, :user_id))
+        @comment.user_id = session[:user_id]
+        if @comment.save 
+            redirect_to sighting_path(@sighting) 
+          else
+            flash.now[:notice] = "Could not create comment."
+            render :new 
+          end
     end 
 
-    # def show 
-    #     @comment = Comment.find_by_id(params[:id])
-    # end
-
-    def destroy 
+    def destroy
         @sighting = Sighting.find(params[:sighting_id])
         @comment = @sighting.comments.find(params[:id])
         @comment.destroy 
         redirect_to sighting_path(@sighting) 
+    end
+
+     private 
+
+    def find_user
+        @user = User.find_by_id(params[:user_id])
+      end
+
+    def can_delete?
+        if !(@sighting.comment.user = session[:user_id])
+          redirect_to sightings_path, alert: "You cannot edit a comment you didn't create"
+        end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_213141) do
+ActiveRecord::Schema.define(version: 2021_07_20_224020) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_213141) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "sighting_id", null: false
+    t.integer "user_id"
   end
 
   create_table "cryptids", force: :cascade do |t|
